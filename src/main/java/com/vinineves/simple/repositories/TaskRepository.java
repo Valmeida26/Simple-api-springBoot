4289ddba99908ddf7e3992d5351ed497af6055bd
Valmeida26 <vinicius.devalmeida@gmail.com>
2024-02-05 23:50:41 -0300

    Create Interfaces User and Task Repository

---
+++ src/main/java/com/vinineves/simple/repositories/TaskRepository.java
@@ -0,0 +1,18 @@
+package com.vinineves.simple.repositories;
+
+import com.vinineves.simple.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByUser_id(Long id);
+
+//    @Query(value = "SELECT t FROM Task t WHERE t.user.id = :id")
+//    List<Task> findByUser_id(@Param("id") Long id);
+}
---
+++ src/main/java/com/vinineves/simple/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.vinineves.simple.repositories;
+
+import com.vinineves.simple.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
